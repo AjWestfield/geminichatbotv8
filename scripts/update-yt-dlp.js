192386f4faed7413c9622c90fb4d2fdb0359964e
Anderson Westfield <ajwestfield8889@gmail.com>
2025-07-07 07:04:35 -0700

    fix: convert update-yt-dlp.js to ES module syntax

--- scripts/update-yt-dlp.js
+++ scripts/update-yt-dlp.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process');
-const { promisify } = require('util');
+import { exec } from 'child_process';
+import { promisify } from 'util';
 const execAsync = promisify(exec);
 
 async function updateYtDlp() {
